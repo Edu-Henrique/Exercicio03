06e555e8d8c6b6d75277a3c8988b5df18f7cff3b
Eduardo Henrique <eduardo.henrique.assuncao.ribeiro@gmail.com>
2024-03-08 22:10:03 -0400

    Adicionado Cadastro de Participante

---
+++ Nivel1/insertParticipante.php
@@ -0,0 +1,15 @@
+<?php
+
+$data = $_POST;
+
+try{
+    $conn = mysqli_connect("localhost", "root", "", "eventos");
+    $sql = "INSERT INTO PARTICIPANTE (ID, NOME, ENDERECO, BAIRRO, CIDADE, TELEFONE, EMAIL)
+            VALUES (DEFAULT, '{$data['nome']}', '{$data['endereco']}', '{$data['bairro']}', {$data['cidade']}, '{$data['telefone']}', '{$data['email']}')";
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
+    echo "Cadastrado com Sucesso! <a href='participanteForm.php'>Voltar</a>";
+} catch(Exception $e)
+{
+    echo "Erro: " . $e->getMessage();
+}
